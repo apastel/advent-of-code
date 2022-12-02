8cb4f6141f1a2f6f91aa8ab64b289645c4599b6a
apastel <alex.r.pastel@gmail.com>
2022-12-01 21:21:39 -0700

    day1

---
+++ 2022/day1_1.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    with open("input/day1.txt") as f:
+        lines = f.read().split("\n\n")
+        elf_calories_list = [elf.split("\n") for elf in lines]
+        elf_calories_list = [[int(x) for x in elf] for elf in elf_calories_list]
+        print(max([sum(elf) for elf in elf_calories_list]))
+        
