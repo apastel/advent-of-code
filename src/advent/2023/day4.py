9d38cf6f17df5c88a63298e311a52ac5a3227cc2
apastel <alex.r.pastel@gmail.com>
2024-01-01 15:42:22 -0700

    day 4 part 2

--- src/advent/2023/day4.py
+++ src/advent/2023/day4.py
@@ -1,4 +1,5 @@
 from advent.load import read_input
+from time import perf_counter as pc
 
 def find_matches(winning_numbers, my_numbers):
     return set(winning_numbers) & set(my_numbers)
@@ -6,16 +7,19 @@ def find_matches(winning_numbers, my_numbers):
 def calculate_points(matches):
     return 2 ** (len(matches) - 1) if matches else 0
 
-data = read_input(group=True)[0]
-total_points = 0
-for card in data:
-    card_number = int(card[card.find(" "):card.find(":")])
-    winning_numbers = [int(num) for num in card[card.find(":")+1:card.find("|")].strip().split(" ") if num]
-    my_numbers = [int(num) for num in card[card.find("|")+1:].strip().split(" ") if num]
-    
-    card_matches = find_matches(winning_numbers, my_numbers)
-    card_points = calculate_points(card_matches)
-    total_points += card_points
-    print(f"Card {card_number} has {len(card_matches)} winning numbers, so it is worth {card_points} points.")
+cards = [[card] for card in read_input(group=True)[0]]
+total_cards = 0
+start_time = pc()
+for card_set in cards:
+    for card in card_set:
+        total_cards += 1
+        card_number = int(card[card.find(" "):card.find(":")])
+        winning_numbers = [int(num) for num in card[card.find(":")+1:card.find("|")].strip().split(" ") if num]
+        my_numbers = [int(num) for num in card[card.find("|")+1:].strip().split(" ") if num]
+        
+        num_matches = len(find_matches(winning_numbers, my_numbers))
+        for x in range(card_number, card_number + num_matches):
+            cards[x].append(cards[x][0])
 
-print(f"The scratchcards are worth {total_points} points in total.")
+print(f"There are now {total_cards} cards in total.")
+print(f"That took {pc() - start_time} seconds, big whoop.")
